6b96cee29855ddceeca41d11fe199e4f83e15653
Raissa Ferreira <rai200890@gmail.com>
2015-08-09 21:50:13 -0300

    Basic authentication for api using access_token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+  before_action :restrict_access
+
+  private
+
+  def restrict_access
+    head :unauthorized if params[:access_token] == ENV["API_KEY_#{Rails.env}"]
+  end
+
 end
